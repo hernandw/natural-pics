47b40cfd2a59f81275dfe02a882be6a6523487b4
Williams Hernandez <hernandw@gmail.com>
2023-03-30 21:04:27 -0300

    :bug: error en galeria

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,18 +1,27 @@
-import React, { useContext } from 'react'
-import { MyContext } from '../context/MyContext'
-import Heart from './Heart'
+import React, { useContext } from "react";
+import { MyContext } from "../context/MyContext";
+import Heart from "./Heart";
 
 const Galeria = () => {
-    const fotos = useContext(MyContext)
+  const  fotos = useContext(MyContext);
+const handleClick = (id)=>{
+    console.log('me diste click en el id', id)
+}
+  
   return (
-    <div className='galeria grid-columns-5 p-3'>
-      {fotos.map(foto =>(
-        <div key={foto.id}>
-        <Heart filled={false} />
-      </div>
+    <div className="galeria grid-columns-5 p-3">
+      {fotos.map((foto) => (
+        <div
+          onClick={() => handleClick(foto.id)}
+          className="foto"
+          key={foto.id}
+          style={{ backgroundImage: `url(${foto.src})` }}
+        >
+          <Heart filled={foto.favorito} />
+        </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Galeria
+export default Galeria;
